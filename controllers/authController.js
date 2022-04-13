1bb6881672c3fe7009a93c32f80f6f00a97c9d51
franco-nahuel-ibanez <franco.nahuel.ibanez@gmail.com>
2022-04-13 02:00:21 -0300

    cerrar sesion

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,3 +7,21 @@ exports.autenticarUsuario = passport.authenticate('local', {
     failureFlash: true,
     badRequestMessage: 'Ambos campos son obligatorios'
 })
+
+//funcion para revisar si el usuario esta logueado
+exports.usuarioAutenticado = (req, res, next) => {
+
+    // si el usuario esta autenticado 
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+    // si no esta autenticado, redirigir
+    return res.redirect('/iniciar-sesion');
+}
+
+exports.cerrarSesion = async(req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/iniciar-sesion');
+    })
+}
